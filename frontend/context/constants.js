45f4df4719404e0cdf44c5174ec8901ca8f8fa3a
cosmatudor <tudor.cosma53@gmail.com>
2024-03-18 14:22:30 +0200

    moved to sepolia testnet

--- frontend/context/constants.js
+++ frontend/context/constants.js
@@ -1,6 +1,6 @@
 import NFTMarketplace from "./NFTMarketplace.json";
 
-export const NFTMarketplaceAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const NFTMarketplaceAddress = "0x12609424c1220f3Abc14038544D92c416447921f";
 // 0x5FbDB2315678afecb367f032d93F642f64180aa3
 // 0x12609424c1220f3Abc14038544D92c416447921f
 export const NFTMarketplaceABI = NFTMarketplace.abi;
